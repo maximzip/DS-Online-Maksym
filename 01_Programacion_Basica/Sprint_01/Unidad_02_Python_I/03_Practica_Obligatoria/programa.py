fb24e42e5dfcf453b65effd6088ed0165a81caa3
maximzip <maksymchaikamc@gmail.com>
2026-03-23 20:26:23 +0100

    Challenge U1 S2
    Ejercicio obligatorio + practicas

---
+++ 01_Programacion_Basica/Sprint_01/Unidad_02_Python_I/03_Practica_Obligatoria/programa.py
@@ -0,0 +1,2 @@
+print("Buenas tardes, bienvenido al servicio de pedido online, \n", 
+      "el precio de nuestra pizzas es de 8,95 para la familiar o de 9,90 para la mediana ¿Cuántas pizzas familiares desea?")
