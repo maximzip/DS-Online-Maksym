c8b0f3c4cb2fd16ad6bf8fe5b7bf80144aa62aef
maximzip <maksymchaikamc@gmail.com>
2026-09-13 16:30:20 +0200

    actualización

---
+++ taller_api_local/app/schemas.py
@@ -0,0 +1,35 @@
+# Crear TrainResponse, MetricsResponse, PredictResponse
+
+from pydantic import BaseModel, Field
+
+class PredictRequest(BaseModel):
+    TV: float = Field(..., ge=0, description='Inversión en publicidad TV (miles de $)')
+    Radio: float = Field(..., ge=0, description='Inversión en publicidad Radio (miles de $)')
+    Newspaper: float = Field(..., ge=0, description='Inversión en publicidad Newspaper (miles de $)')
+
+    class Config:
+        kson_schema_extra ={
+            'exemple': {'TV': 230.1, 'Radio':37.8, 'Newspaper':69.2}
+        }
+# cuales son las columnas y que datos tienes
+
+
+class PredictResponse(BaseModel):
+    predicted_sales : float
+    model_version : str
+#solo predicciones y version
+
+
+class TrainResponse(BaseModel):
+    message : str
+    model_version : str
+    n_samples : int
+    metrics : dict
+
+
+
+class MetricsResponse(BaseModel):
+    model_version : str | None
+    trained_at : str | None
+    n_samples : int | None
+    metrics : dict | None
